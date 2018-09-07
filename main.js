4e9d01d3c2da65a78fa18c55e10c7982fca2f0d8
Misha Holzman <mcholzman1@gmail.com>
2018-09-07 12:17:10 -0400

    added more animation

--- main.js
+++ main.js
@@ -11,62 +11,127 @@ let userScore = 0
 let goalieScore = 0
 
 
-function startGame() {
 	document.querySelector(".leftGoal").addEventListener("click", kickBallLeft);
+	document.querySelector(".leftGoal").addEventListener("click", animateBallLeft);
 	document.querySelector(".middleGoal").addEventListener("click", kickBallMiddle);
-	document.querySelector(".rightGoal").addEventListener("click", kickBallRight);
+	document.querySelector(".rightGoal").addEventListener("click", animateBallRight);
+
+
+	function animateBallLeft() {
+			let ballElement = document.querySelector('#stillBall')
+   			ballElement.classList.toggle("ballLeft");
+		}
 
 	function kickBallLeft() {
 		const keeperNumber = Math.floor(Math.random() * 2) + 1;
 		if (leftGoalNumber !== keeperNumber) {
 			console.log('GOAL!!')
-			userScore += 1;
+			userScore++;
 			if (userScore === 5) {
 				alert('You won')
 			}
 		} else {
 			console.log('Save :(')
-			goalieScore += 1;
+			goalieScore++;
 			if (goalieScore === 5) {
 				alert('You lost')
 			}
 		}
 	}
 
+
 	function kickBallMiddle() {
+		function animateBallMiddle() {
+			let ballElement = document.querySelector('#stillBall')
+   			ballElement.classList.toggle("ballMiddle");
+		}
+		animateBallMiddle()
+
 		const keeperNumber = Math.floor(Math.random() * 2) + 1;
 		if (middleGoalNumber !== keeperNumber) {
 			console.log('GOAL!!')
-			userScore += 1;
+			userScore++;
 			if (userScore === 5) {
 				alert('You won')
 			}
 		} else {
 			console.log('Save :(')
-			goalieScore += 1;
+			goalieScore++;
 			if (goalieScore === 5) {
 				alert('You lost')
 			}
 		}
 	}
 
-	function kickBallRight() {
+
+	function animateBallRight() {
+		let ballElement = document.querySelector('#stillBall')
+   		ballElement.classList.toggle("ballRight");
+
+   		function kickBallRight() {
 		const keeperNumber = Math.floor(Math.random() * 2) + 1;
 		if (rightGoalNumber !== keeperNumber) {
 			console.log('GOAL!!')
-			userScore += 1;
+			userScore++;
 			if (userScore === 5) {
 				alert('You won')
 			}
 		} else {
 			console.log('Save :(')
-			goalieScore += 1;
+			goalieScore++;
 			if (goalieScore === 5) {
 				alert('You lost')
 			}
 		}
 	}
-}
-startGame()
+		kickBallRight()
+	}
+	
+
+	
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector(".ballContainer").addEventListener('keydown', animateBallLeft)
+
+// function animateBallLeft (evt) {
+// 	// const keyName = evt.key;
+// 	console.log(evt)
+// 	const sideStickAudio = document.querySelector('.sideStick');
+// 	if (event.code == 'KeyZ') {
+// 		sideStickAudio.play()
+// 	}
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
