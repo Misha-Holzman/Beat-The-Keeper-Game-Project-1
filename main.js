d33e5717e8f67a1deab21b9493bdca7d56b93fa0
Misha Holzman <mcholzman1@gmail.com>
2018-09-06 23:14:37 -0400

    added graphics in css and started with animation. Added scoring logic.

--- main.js
+++ main.js
@@ -1,81 +1,72 @@
 
 
 
-
-
-
-
 const leftGoalBtn = document.querySelector('.leftGoal');
 const rightGoalBtn = document.querySelector('.middleGoal');
 const middleGoalBtn = document.querySelector('.rightGoal');
 const leftGoalNumber = 1;
 const rightGoalNumber = 2;
-const middleGoalNumber = 3;
-// const keeperNumber = Math.floor(Math.random() * 3) + 1;
+const middleGoalNumber = 2;
+let userScore = 0
+let goalieScore = 0
 
 
-document.querySelector(".leftGoal").addEventListener("click", kickBallLeft);
-document.querySelector(".middleGoal").addEventListener("click", kickBallMiddle);
-document.querySelector(".rightGoal").addEventListener("click", kickBallRight);
+function startGame() {
+	document.querySelector(".leftGoal").addEventListener("click", kickBallLeft);
+	document.querySelector(".middleGoal").addEventListener("click", kickBallMiddle);
+	document.querySelector(".rightGoal").addEventListener("click", kickBallRight);
 
+	function kickBallLeft() {
+		const keeperNumber = Math.floor(Math.random() * 2) + 1;
+		if (leftGoalNumber !== keeperNumber) {
+			console.log('GOAL!!')
+			userScore += 1;
+			if (userScore === 5) {
+				alert('You won')
+			}
+		} else {
+			console.log('Save :(')
+			goalieScore += 1;
+			if (goalieScore === 5) {
+				alert('You lost')
+			}
+		}
+	}
 
-function kickBallLeft() {
-	const keeperNumber = Math.floor(Math.random() * 3) + 1;
-	if (leftGoalNumber !== keeperNumber) {
-		console.log('GOAL!!')
-	} else {
-		console.log('Save :(')
+	function kickBallMiddle() {
+		const keeperNumber = Math.floor(Math.random() * 2) + 1;
+		if (middleGoalNumber !== keeperNumber) {
+			console.log('GOAL!!')
+			userScore += 1;
+			if (userScore === 5) {
+				alert('You won')
+			}
+		} else {
+			console.log('Save :(')
+			goalieScore += 1;
+			if (goalieScore === 5) {
+				alert('You lost')
+			}
+		}
+	}
+
+	function kickBallRight() {
+		const keeperNumber = Math.floor(Math.random() * 2) + 1;
+		if (rightGoalNumber !== keeperNumber) {
+			console.log('GOAL!!')
+			userScore += 1;
+			if (userScore === 5) {
+				alert('You won')
+			}
+		} else {
+			console.log('Save :(')
+			goalieScore += 1;
+			if (goalieScore === 5) {
+				alert('You lost')
+			}
+		}
 	}
 }
+startGame()
 
-function kickBallMiddle() {
-	const keeperNumber = Math.floor(Math.random() * 3) + 1;
-	if (middleGoalNumber !== keeperNumber) {
-		console.log('GOAL!!')
-	} else {
-		console.log('Save :(')
-	}
-}
-
-function kickBallRight() {
-	const keeperNumber = Math.floor(Math.random() * 3) + 1;
-	if (rightGoalNumber !== keeperNumber) {
-		console.log('GOAL!!')
-	} else {
-		console.log('Save :(')
-	}
-}
-
-
-
-
-
-
-
-
-// leftGoalBtn.addEventListener('click',(event) => {
-// 	let targetBtn = event.target.innerText;
-// 	return leftGoalNumber
-// });
-
-
-
-// document.addEventListener('click', shootLeftGoal)
-
-// function shootLeftGoal (evt) {
-// 	// const keyName = evt.key;
-// 	console.log(evt)
-// 	const leftGoal = document.querySelector('.sideStick');
-// 	if (event.code == 'KeyZ') {
-// 		leftGoal.play()
-// 	}
-// };
-
-
-// let targetBtn = event.target.innerText;
-// 	// if button innerHTML === kick
-// 	// then play audio file
-// 	if (targetBtn == 'left-goal') { 
-// 		Math.floor(Math.random() * 1) + 1;
-		
 
